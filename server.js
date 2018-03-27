9be1ce31b09e63ae7f50e13a9ce267b92d73e279
madhushanka Deshan <madhushankadeshan96@gmail.com>
2018-03-27 14:42:24 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = `${now} : ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+
+
+});
+
+hbs.registerHelper('getCurrentYear',() => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt',(text) => {
+  return text.toUpperCase();
+});
+
+app.get('/',(req, res) => {
+
+  // res.send('Hello express!');
+  res.render('home.hbs',{
+    pageTitle:"Home Page",
+    welcomeMessage:'Welcome to my Site'
+  });
+
+  });
+
+  app.get('/about',(req, res) => {
+    res.render('about.hbs', {
+      pageTitle:'About Page'
+    });
+  });
+
+  app.get('/bad',(req, res) => {
+    res.send({
+      errorMessage: 'Unable to handle request'
+    });
+  });
+
+
+app.listen(port ,() => {
+  console.log(`Server is up on port ${port}`);
+});
